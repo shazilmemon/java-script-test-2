0b426b8a85a99c288c7c4890d58eed1999ddd363
Shazil <shazilmemon6@gmail.com>
2022-12-22 14:33:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,146 @@
+// java script test 2 chapter 13 to 15
+
+// answer no 1
+
+// var arr =["qualifications"];
+// console.log(arr[0])
+
+// var arr = ["1) SSC"];
+// console.log(arr[0])
+
+// var arr = ["2) HSC"];
+// console.log(arr[0])
+
+// var arr = ["3) BCS"];
+// console.log(arr[0])
+
+// var arr = ["4) BS"];
+// console.log(arr[0])
+
+
+// var arr = ["5) B.COM"];
+// console.log(arr[0])
+
+// var arr = ["6) MS"];
+// console.log(arr[0])
+
+
+// var arr = ["7) M.PHIL"];
+// console.log(arr[0])
+
+// var arr = ["8) PHD"];
+// console.log(arr[0])
+
+// answer no 2
+
+
+// var arr = ["1) score of michael is 500. percentage: 95% "];
+// console.log(arr[0])
+
+// var arr = ["2) score of john is 400. percentage: 75% "];
+// console.log(arr[0])
+
+// var arr = ["3) score of tony is 300. percentage: 65% "];
+// console.log(arr[0])
+
+
+// answer no 3
+
+// var arr = ["cities list"];
+// console.log(arr[0]) 
+
+
+// var arr = ["karachi,lahore,quetta,islamabad,peshawar"];
+// console.log(arr[0])
+
+// var arr =["selected cities list"];
+// console.log(arr[0])
+
+// var arr =["islamabad,quetta"];
+// console.log(arr[0])
+
+
+// answer no 4
+
+
+// var arr =["this","is","my","cat"];
+// console.log(arr.join (" "))
+
+// answer no 5
+
+// var arr = ["keyboard","mouse","printer","monitor"];
+// console.log(arr.shift());
+
+// answer no 6
+
+// var arr = ["keyboard","mouse","printer","monitor"];
+// console.log(arr.pop());
+
+
+
+// answer no 7
+
+// var select = document.getElementById("selectNumber"); 
+// var options = ["apple", "samsung", "motrolla", "nokia", "sony","haier"]; 
+
+// // Optional: Clear all existing options first:
+// select.innerHTML = "";
+// // Populate list with options:
+// for(var i = 0; i < options.length; i++) {
+//     var opt = options[i];
+//     select.innerHTML += "<option value=\"" + opt + "\">" + opt + "</option>";
+// }
+
+
+// answer no 8
+
+// var notation =["johny","john","ken"];{
+
+//     alert(notation[0])
+// }
+
+// answer no 9
+
+
+
+// var array1 = [];
+
+
+// let array2 = [];
+
+// let array3 = new Array();
+
+// console.log({array1, array2, array3});
+
+
+// answer no 10 not understand
+
+// answer no 11 not understand
+
+// answer no 12 not understand
+
+// answer no 13 not understand
+
+// answer no 14 not understand
+
+// var city = prompt('enter your city name')
+// city = city.toUpperCase()
+
+// var arr = ['KARACHI','ISLAMABAD','PESHAWAR']
+// for(var i=0; i<arr .length; i++){
+
+//     if(arr[i] === city){
+
+//         alert('city has found')
+//     }else{alert('city han no found')}
+// }
+
+
+
+
+
+
+
+
+
+
